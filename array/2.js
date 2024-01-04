9bf6d5d312bbe2d1711d723196023d7c4ff1ff6d
Moiz ansari <ansarimoiz979@gmail.com>
2024-01-04 19:32:37 +0530

    2.js-> pop() or shift() method ka use krkr array me  element remove kiye,  3.js->splice() method ka use krkr array ke start or last me member/element add kiye he

---
+++ array/2.js
@@ -0,0 +1,96 @@
+//remove elements  from array
+/*
+use of pop() and shift method
+
+*/
+
+
+//create array with some elements
+let arr = [ 10, 20, 30, 40, 50, 60, 70]
+//print size of the array
+console.log("size of the arr is : "+ arr.length)
+
+
+//pop() method : pop() ki help se array ke last member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr.pop()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//pop() method : pop() ki help se array ke last member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr.pop()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//pop() method : pop() ki help se array ke last member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr.pop()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+
+//pop() method : pop() ki help se array ke last member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr.pop()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+console.log("\n\n\n");
+//-------------------------------------------------------------------
+/*
+shift() method : shift() method se array ke start ke member remove hoge
+*/
+//create array with some elements
+let arr1 = [ 100, 200, 300, 400, 500, 600, 700]
+//print size of the array
+console.log("size of the arr is : "+ arr1.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+//shift() method : shift() ki help se array ke start ke member ko array se remove kr skte he
+console.log("\nbefore( phle ) 1-> ", arr);
+arr1.shift()
+console.log("after (bad me) 1-> ", arr);
+console.log("size after removing element", arr.length)
+
+
+
---
+++ array/3.js
@@ -0,0 +1,91 @@
+/*
+abtak hmne array ke start or end me new member add or remove kiye he 
+
+ab ham array ke middile me new member add or remove krenge 
+ham is file me single member add or remove krenge
+next file me multiple member add or delete krenge
+
+splice() method ka use hoga
+
+splice method me teen parameter hote he
+1-> start : index jha se hame value insert krna start krni he
+2-> deletecount :  jitne element htane/replace krne he
+3-> new elements : elements saprated by comme 
+
+splice(startindex)
+splice(startindex, deletecount )
+splice(startindex, deletecount, item1)
+splice(startindex, deletecount, item1, item2)
+splice(startindex, deletecount, item1, item2, item3, ----- , item-N )
+
+*/
+
+//neeche wale example me array ke start me new element add krne ka code he
+let arr = []
+console.log("1-> arr",arr);
+console.log("1-> length of the array ", arr.length)
+
+arr.splice(0, 0, 100);
+console.log("\n2-> arr",arr);
+console.log("2-> length of the array ", arr.length)
+
+
+arr.splice(0, 0, 200);
+console.log("\n3-> arr",arr);
+console.log("3-> length of the array ", arr.length)
+
+
+
+arr.splice(0, 0, 300);
+console.log("\n4-> arr",arr);
+console.log("4-> length of the array ", arr.length)
+
+
+
+arr.splice(0, 0, 400);
+console.log("\n5-> arr",arr);
+console.log("5-> length of the array ", arr.length)
+
+
+
+arr.splice(0, 0, 500);
+console.log("\n6-> arr",arr);
+console.log("6-> length of the array ", arr.length)
+
+
+
+//neeche wale example me array ke end me new element add krne ka code he
+/*
+last me element add krne ke liye negative number dena hota he
+yeh tareeka pop() method ki trh kam krenga
+*/
+let arr1 = []
+console.log("1-> arr",arr1);
+console.log("1-> length of the array ", arr1.length)
+
+arr1.splice(-1, 0, 100);
+console.log("\n2-> arr",arr1);
+console.log("2-> length of the array ", arr1.length)
+
+
+arr1.splice(-1, 0, 200);
+console.log("\n3-> arr",arr1);
+console.log("3-> length of the array ", arr1.length)
+
+
+
+arr1.splice(-1, 0, 300);
+console.log("\n4-> arr",arr1);
+console.log("4-> length of the array ", arr1.length)
+
+
+
+arr1.splice(-1, 0, 400);
+console.log("\n5-> arr",arr1);
+console.log("5-> length of the array ", arr1.length)
+
+
+
+arr1.splice(-1, 0, 500);
+console.log("\n6-> arr",arr1);
+console.log("6-> length of the array ", arr1.length)
